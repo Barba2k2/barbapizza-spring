6b214b966b6adfe3f9f5d0e558d64a775216b91c
Lorenzo DZ <lorenzo.dz@hotmail.com>
2025-04-22 10:19:07 -0300

    First spring project

---
+++ src/main/java/com/barbapizza/barbapizza/BarbapizzaApplication.java
@@ -0,0 +1,13 @@
+package com.barbapizza.barbapizza;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BarbapizzaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BarbapizzaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/barbapizza/barbapizza/BarbapizzaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.barbapizza.barbapizza;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BarbapizzaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
