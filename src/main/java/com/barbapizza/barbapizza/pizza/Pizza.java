5d9151ac0abf509a6e622e0324c213abec2aa781
Lorenzo DZ <lorenzo.dz@hotmail.com>
2025-04-23 10:07:23 -0300

    Enhance Pizza entity with JPA annotations and update fields for Sabor and Tamanho

--- src/main/java/com/barbapizza/barbapizza/pizza/Pizza.java
+++ src/main/java/com/barbapizza/barbapizza/pizza/Pizza.java
@@ -1,10 +1,21 @@
 package com.barbapizza.barbapizza.pizza;
 
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table(name = "Pizza")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class Pizza {
+  @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
   private Long id;
   private String nome;
-  private Sabor sabor;
   private double price;
-  private Tamanho tamanho;
   private boolean disponivel;
+  @Enumerated(EnumType.STRING)
+  private Tamanho tamanho;
+  @Enumerated(EnumType.STRING)
+  private Sabor sabor;
 }
